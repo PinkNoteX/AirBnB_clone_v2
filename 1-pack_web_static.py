b90f1103ac21b55b671875eb172b18a09b78d1d0
PinkNoteX <omarfarghaly8@gmail.com>
2024-02-01 15:30:30 -0800

    Task 1 done

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# generate a .tgz archive from web_static
+from fabric.api import *
+import os
+from datatime import datatime
+import tarfile
+
+
+def do_pack():
+    """ do_pack func """
+    try:
+        p = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S")
+        local('mkdir -p versions')
+        local("tar -cvzf versions/{}.tgz {}".format(p, "web_static/"))
+        s = os.path.getsize("./versions/{}.tgz".format(p))
+        print("web_static packed: versions/{}.tgz -> {}Bytes".format(p, s))
+    except Exception:
+        return None
