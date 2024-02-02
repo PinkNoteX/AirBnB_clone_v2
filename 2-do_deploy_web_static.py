9c50bed9072aaac1893f70cd29f3f365c4459123
PinkNoteX <omarfarghaly8@gmail.com>
2024-02-01 16:16:18 -0800

    Task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# distribute an archive to web servers
+from fabric.api import *
+import os
+from datetime import datetime
+import tarfile
+
+
+env.hosts = ['34.204.101.190', '100.25.197.195']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_deploy(archive_path):
+    """ do_deploy func """
+    f = archive_path.split('/')[1]
+    try:
+        put(archive_path, '/tmp/{}'.format(f))
+        run('mkdir -p /data/web_static/releases/{}'.format(f))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(f, f))
+        run('rm /tmp/{}'.format(f))
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(f, f))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(f))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/\
+                /data/web_static/current'.format(f))
+        print("New version deployed!")
+        return True
+    except Exception:
+        print("New version not deployed...")
+        return False
