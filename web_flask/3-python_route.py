81a5967341f801c9d8eb71f70684b1a98f4ac0f8
PinkNoteX <omarfarghaly8@gmail.com>
2024-02-18 15:34:12 -0800

    Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" flask server """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """ hello flask """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ hbnb """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """ /c/<text> """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>')
+@app.route('/python')
+def python_text(text='is cool'):
+    """ /python/<text> """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run()
+    app.url_map.strict_slashes = False
